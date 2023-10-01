d8bbce18e27ae9fefef17458c2aac5cf1ecbda75
floington500 <zachappella@gmail.com>
2023-10-01 12:42:32 -0400

    add headers to their own directory

---
+++ include/math.h
@@ -0,0 +1,10 @@
+#ifndef MATH_H
+#define MATH_H
+
+double accelerationForm(double, double);
+
+double velocityForm(double, double, double);
+
+double timeForm(double, double, double);
+
+#endif // MATH_H
