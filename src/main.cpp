b6c49bed98d9e5c3834ae1e729cb0ba2b63012fd
floington500 <zachappella@gmail.com>
2023-10-01 22:04:10 -0400

    final design

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,6 @@
 
 constexpr double GRAVITY = 9.8;
 
-void printColumn(int n)
-{
-	for (int i = 0; i < n; ++i)
-	{
-		std::cout << "# \n";
-	}
-}
-
 /**
  * Prepares the terminal emulator for the simulation.
  */
@@ -23,6 +15,7 @@ void setup()
 	std::cout.setf(std::ios::unitbuf); // turn off stdout buffer
 	std::cout.sync_with_stdio(false);
 	std::cout << "\033[?25l"; // disable cursor blinking
+	std::cout << "\033[0;1H"; // move cursor to [0][1]
 }
 
 // usage:
@@ -42,17 +35,13 @@ int main(int argc, char **argv)
 		computeVelocity(GRAVITY, d)
 	);
 
-	// print the column for the rock to drop from
-	printColumn(d);
-	std::cout << "\033[0;2H"; // move cursor to [0][2]
-
 	for (auto t = vt.rbegin(); t != vt.rend(); ++t) {
 		std::cout << "*";
 	
 		usleep(*t * 1000000);
 
-		std::cout << "\b";
-		std::cout << "\033[B";
+		std::cout << "\b \b"; // erase rock
+		std::cout << "\033[B"; // move cursor down
 	}
 
 	return 0;
