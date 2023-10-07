25a67fcd40f916e7d37d1f873a60acca38e9dbbd
floington500 <zachappella@gmail.com>
2023-10-06 23:34:10 -0400

    update build script to give it more flexibility

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 #
-g++ -c src/main.cpp -Iinclude -o build/main.o
+# ENTER PROGRAM NAME TO BUILD LIKE SO:
+# ./build.sh <target>
+#
+program_name=$1
+obj="${program_name}.o"
+
+g++ -c "src/${program_name}.cpp" -Iinclude -o "build/${obj}"
 g++ -c src/math.cpp -Iinclude -o build/math.o
 
-g++ build/main.o build/math.o -o build/freefall
+g++ "build/${obj}" build/math.o -o "build/${program_name}"
